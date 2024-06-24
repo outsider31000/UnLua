76e1ac5aad2a0f6989f735f79785e7f5b7336dfe
GD <nkari82@gmail.com>
2024-06-24 12:42:49 +0900

    Cleanup

--- Types/@types/UE/Math.d.ts
+++ Types/@types/UE/Math.d.ts
@@ -1,6 +1,6 @@
-type FVector = number & Vector;
+type FVector = number & Vector3;
 
-declare class Vector {
+declare class Vector3 {
   X: number;
   Y: number;
   Z: number;
@@ -15,41 +15,85 @@ declare class Vector {
   ToRotator(): FRotator;
   ToQuat(): FQuat;
   Size(): number;
-  Size2D(): unknown;
+  Size2D(): number;
   SizeSquared(): number;
-  SizeSquared2D(): unknown;
+  SizeSquared2D(): number;
   IsNormalized(): boolean;
   CosineAngle2D(): unknown;
   RotateAngleAxis(): unknown;
 
-  static Dist(a: FVector): number;
-  static Dist2D(a: unknown): unknown;
-  static DistSquared(a: FVector): number;
-  static DistSquared2D(a: unknown): unknown;
+  static Dist(a: FVector, b: FVector): number;
+  static Dist2D(a: FVector2D, b: FVector2D): number;
+  static DistSquared(a: FVector, b: FVector): number;
+  static DistSquared2D(a: FVector2D, b: FVector2D): number;
 }
 
 /** @noSelf @customName UE.FVector **/
 declare function FVector(x?: number, y?: number, z?: number): FVector;
 
-declare class FRotator {
-  constructor();
-  constructor(InF: number);
-  constructor(InPitch: number, InYaw: number, InRoll: number);
-  constructor(Param1: number);
-  constructor(Quat: FQuat);
+type FVector4 = number & Vector4;
+
+declare class Vector4 {
+  X: number;
+  Y: number;
+  Z: number;
+  W: number;
+}
+
+type FVector2D = Vector2;
+declare class Vector2 {
+  X: number;
+  Y: number;
+}
+
+type FQuat = number & Quat;
+
+declare class Quat {
+  X: number;
+  Y: number;
+  Z: number;
+  W: number;
+
+  Set(x: number, y: number, z: number, w: number): void;
+  FromAxisAndAngle(axis: FVector, angle: number): void;
+  GetNormalized(): FQuat;
+  IsNormalized(): boolean;
+  Size(): number;
+  SizeSquared(): number;
+  ToAxisAndAngle(): unknown; // multi return: FVector, number
+  Inverse(): FQuat;
+  RotateVector(): FVector;
+  UnrotateVector(): FVector;
+  GetAxisX(): FVector;
+  GetAxisY(): FVector;
+  GetAxisZ(): FVector;
+  GetForwardVector(): FVector;
+  GetRightVector(): FVector;
+  GetUpVector(): FVector;
+  Slerp(a: FQuat, alpha: number): FQuat;
+  ToEuler(): FVector;
+  ToRotator(): FRotator;
+}
+
+/** @noSelf @customName UE.FQuat **/
+declare function FQuat(x?: number, y?: number, z?: number, w?: number): FQuat;
+
+/** @noSelf @customName UE.FQuat **/
+declare function FQuat(R: FRotator): FQuat;
+
+/** @noSelf @customName UE.FQuat **/
+declare function FQuat(Axis: FVector, AngleRad: number): FQuat;
+
+type FRotator = Rotator;
+declare class Rotator {
   Pitch: number;
   Yaw: number;
   Roll: number;
 }
 
-declare class FQuat {
-  constructor();
-  constructor(Param1: number);
-  constructor(InX: number, InY: number, InZ: number, InW: number);
-  constructor(R: FRotator);
-  constructor(Axis: FVector, AngleRad: number);
-  X: number;
-  Y: number;
-  Z: number;
-  W: number;
+type FTransform = Transform;
+declare class Transform {
+  Rotation: FQuat;
+  Translation: FVector;
+  Scale: FVector;
 }
